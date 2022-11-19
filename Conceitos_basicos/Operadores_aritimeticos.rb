d171f92bbd3fd5ee8953cc33cd05eec857fadbcb
lariissarodrigues <lrs.larissa.rodrigues@gmail.com>
2022-11-19 09:06:43 -0300

    Operadores artimeticos e Relacionais

---
+++ Conceitos_basicos/Operadores_aritimeticos.rb
@@ -0,0 +1,37 @@
+#Operadores matematicos e relacionais
+
+
+
+# + -> soma
+# - subtração
+# / -> divisão
+# * -> multiplicar
+# ** -> potencia
+# % -> resto da divisão
+
+
+
+a = 10
+b = 30
+
+
+puts a + b
+puts a - b
+puts a * b
+puts a / b
+puts a ** b
+puts a % b
+
+
+#RELACIONAIS
+#  == -> igual
+#  != -> diferente
+#  > -> maior que ou >= maior igual que
+#  < -> menor que ou <= menor iqual que
+
+puts a == b
+puts a != b
+puts a >  b
+puts a <  b
+puts a >= b
+puts a <= b
