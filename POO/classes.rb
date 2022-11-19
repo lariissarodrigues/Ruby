e87c3e4c7910ce4f5c9d7d07a713166203f5bca1
lariissarodrigues <lrs.larissa.rodrigues@gmail.com>
2022-11-19 16:25:43 -0300

    Classes e Metodos

---
+++ POO/classes.rb
@@ -0,0 +1,10 @@
+class Pessoa
+
+  #criando uma classe
+  #inicializando um metodo
+
+  def andar
+    puts "andando...."
+  end
+
+end
---
+++ POO/main.rb
@@ -0,0 +1,6 @@
+require_relative 'classes' #chamando a classe pessoa em outro arquivo
+
+p = Pessoa.new
+p.andar
+
+
