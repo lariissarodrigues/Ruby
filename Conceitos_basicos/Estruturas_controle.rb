3a94c8002e7c70e2c92d38b2ef33ebdff20ea7f6
lariissarodrigues <lrs.larissa.rodrigues@gmail.com>
2022-11-19 09:18:12 -0300

    Estruturas de Controle

---
+++ Conceitos_basicos/Estruturas_controle.rb
@@ -0,0 +1,27 @@
+#Estrura de controle/Repetição
+
+#WHILE
+
+a = 10
+i = 0
+
+
+while i < a do
+  i = i + 1
+  puts a
+end
+
+
+#EACH
+
+x = 10
+list = [2, 3, 4, 4, 2, 5]
+
+(1..5).each do |cont|
+  puts cont
+end
+
+list.each do |cont|
+  puts cont
+end
+
