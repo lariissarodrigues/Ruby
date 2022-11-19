ca45adc523738342a3a83a610837a39e83dba583
lariissarodrigues <lrs.larissa.rodrigues@gmail.com>
2022-11-19 17:02:17 -0300

    Classe

--- POO/classes.rb
+++ POO/classes.rb
@@ -1,4 +1,10 @@
 class Pessoa
+  #Construtor do ruby
+
+  def initialize(nome, sobrenome)
+    @nome = nome #atributos do objeto. Para definir variaveis de instancia é preciso usar o @
+    @sobrenome = sobrenome
+  end
 
   #criando uma classe
   #inicializando um metodo
@@ -6,5 +12,23 @@ class Pessoa
   def andar
     puts "andando...."
   end
+
+  def nome(nome) #metodos com parametros
+    puts "Olá #{nome}"
+  end
+
+  def nome1(nome = "pessoa", sobrenome = "...")
+    puts("Olá #{nome} #{sobrenome}")
+  end
+
+  #Usando o sel = this do java, ou seja, serve para referencia o proprio objeto
+  def meu_id()
+    "Meu id é: #{self.object_id}"
+  end
+
+  #Gets/Set
+  attr_accessor :nome #gets e set
+  attr_accessor :sobrenome
+
 
 end
